9d63349d3e528e288c387e84eedc6c858e8557fc
jithn-jith <jithinmkp93@gmail.com>
2025-02-23 22:49:50 +0530

    retrieving relevant data from database

--- Retrieving relevant data from database.py
+++ Retrieving relevant data from database.py
@@ -1,9 +1,11 @@
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain_openai.embeddings import OpenAIEmbeddings
-from langchain_community.vectorstores import Chroma
 from langchain_community.chains import PebbloRetrievalQA
 from langchain_openai.chat_models import ChatOpenAI
+from langchain_chroma.vectorstores import Chroma
+from langchain.chains import LLMChain
+from langchain.prompts import ChatPromptTemplate,SystemMessagePromptTemplate,HumanMessagePromptTemplate
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -23,6 +25,22 @@ retriver = db.as_retriever(
     search_type="mmr", 
     search_kwargs={"k": 1, "fetch_k": 5}
 )
-
-result = retriver.invoke("tell me one interesting thing about english language?")
-print(result)
+user_query = "tell me one interesting thing about english language?"
+result = retriver.invoke(user_query)[0].page_content
+system_prompt = f"You are a helpful assistant that goes through a context paragraph and selects the best answer based on a users query. The context is {result} "
+prompt = ChatPromptTemplate(
+    input_variables = ["system_prompt","user_query"],
+    messages=[
+        SystemMessagePromptTemplate.from_template("{system_prompt}"),
+        HumanMessagePromptTemplate.from_template("{user_query}")
+    ]
+)
+chain = LLMChain(
+    llm = chat,
+    prompt = prompt,
+)
+result = chain({
+        "system_prompt":system_prompt,
+        "user_query":user_query
+    })
+print(result["text"])
